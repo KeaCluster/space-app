f429dcfdf02710b91b415eec74e1b0ba967bc81b
KeaCluster <zzabdiel29@gmail.com>
2022-08-15 09:51:51 -0500

    nav changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-import Header from './Components/Header/Header';
+
+import ResponsiveAppBar from './Components/AppBar/AppBar.js';
 import Navbar from './Components/Navbar/Navbar';
 
-
+ 
 function App() {
   // class = "";
   //  className = "";
   // Todos los elementos deben estar en una etiqueta padre
-
+  const data = {
+    name: "Griselo",
+    lastName: "Philip"
+  };
+  
   return (
     <div className="App">
-      <Navbar />
-      <Header />
+      <ResponsiveAppBar />
+      <Navbar {...data}/>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,25 @@
-const Navbar = () => {
-    return (
-      <nav>
+import './Navbar.css';
+
+const Navbar = ({name, lastName}) => {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
         <div className="nav-img">
-            <img src=""></img>
+          <img src=""></img>
         </div>
         <div className="nav-links">
-            <a>Main</a>
-            <a>Contacto</a>
-            <a>About</a>
+          <a>{name}</a>
+          <a>{lastName}</a>
+          <a>About</a>
         </div>
-      </nav>  
-    )
+      </div>
+    </nav>
+  )
+}
+
+Navbar.defaultProps = {
+  name: "Nombre",
+  lastName: "Apellido"
 }
 
 export default Navbar;
