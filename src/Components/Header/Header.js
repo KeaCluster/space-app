e3caf9cd3594a2a9b306b72386a0ca60522bed64
KeaCluster <zzabdiel29@gmail.com>
2022-08-15 07:50:03 -0500

    initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <>
+            <h1>Hola tengo sueño</h1>
+            <p>Vamos a dar clases</p>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+      <nav>
+        <div className="nav-img">
+            <img src=""></img>
+        </div>
+        <div className="nav-links">
+            <a>Main</a>
+            <a>Contacto</a>
+            <a>About</a>
+        </div>
+      </nav>  
+    )
+}
+
+export default Navbar;
